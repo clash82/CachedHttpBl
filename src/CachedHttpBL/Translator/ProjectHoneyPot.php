9377246e7bc2f15fcd8028c6e5e5b772d935b4f4
Rafał Toborek <kontakt@toborek.info>
2019-10-27 18:35:09 +0100

    Added fixes regarding CodeClimate report

--- src/CachedHttpBL/Translator/ProjectHoneyPot.php
+++ src/CachedHttpBL/Translator/ProjectHoneyPot.php
@@ -29,42 +29,53 @@ class ProjectHoneyPot implements Translator
     {
         $threat = $this->response->getThreat();
 
+        $message = '1,000,000 [msg/day]';
+
         if ($threat < 26) {
-            return '100 [msg/day]';
+            $message = '100 [msg/day]';
         }
 
         if ($threat < 51) {
-            return '10,000 [msg/day]';
+            $message = '10,000 [msg/day]';
         }
 
-        return '1,000,000 [msg/day]';
+        return $message;
     }
 
     public function getTypeMeaningDescription(): string
     {
+        $message = 'Unknown';
+
         switch ($this->response->getTypeMeaning()) {
             case 0:
-                return 'Search Engine';
+                $message = 'Search Engine';
+                break;
 
             case 1:
-                return 'Suspicious';
+                $message = 'Suspicious';
+                break;
 
             case 2:
-                return 'Harvester';
+                $message = 'Harvester';
+                break;
 
             case 4:
-                return 'Comment Spammer';
+                $message = 'Comment Spammer';
+                break;
 
             case 5:
-                return 'Suspicious & Comment Spammer';
+                $message = 'Suspicious & Comment Spammer';
+                break;
 
             case 6:
-                return 'Harvester & Comment Spammer';
+                $message = 'Harvester & Comment Spammer';
+                break;
 
             case 7:
-                return 'Suspicious & Harvester & Comment Spammer';
+                $message = 'Suspicious & Harvester & Comment Spammer';
+                break;
         }
 
-        return 'Unknown';
+        return $message;
     }
 }
